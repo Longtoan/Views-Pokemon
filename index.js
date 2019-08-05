18501102dd275023149c1f02b9d2fb76d977b0ad
Long Toan <longtoan.0308@gmail.com>
2019-08-05 19:05:29 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const mongooose = require('mongoose');
+const mongooose = require("mongoose");
 const app = express();
-
-
+const cors = require('cors');
 //import Router
 const postRouter = require("./router/post");
 
@@ -10,16 +9,20 @@ const bodyParse = require("body-parser"); //format data
 
 require("dotenv/config");
 
+app.use(express.static("public"));
+app.use(cors('*'));
 app.use(bodyParse.json());
+
 // connect to database
-mongooose.connect(process.env.MongoConnect, { useNewUrlParser: true },(err) =>{
-  if(err) console.log(err)
-  console.log('connected!');
- })
+mongooose.connect(process.env.MongoConnect, { useNewUrlParser: true }, err => {
+  if (err) console.log(err);
+  else console.log("connected!");
+});
 
 app.use("/todos", postRouter);
 
 app.get("/", (req, res) => {
+  
   res.send("Home");
 });
 
